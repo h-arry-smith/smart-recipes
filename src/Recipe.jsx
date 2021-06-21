9f6b85ba7cf4b115c18e67553d4feee87ea8f5a4
Harry Smith <harry@hsmith.net>
2021-06-21 09:50:07 +0100

    Handle JSON Loading

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -2,24 +2,32 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const Recipe = () => {
-  const [recipe, setRecipe] = useState({});
+  const [recipe, setRecipe] = useState(null);
   let { slug } = useParams();
   
   useEffect(() => {
     async function fetchRecipeJSON() {
-      let response = await fetch(`../recipes/${slug}.json`)
+      let response = await fetch(`../../recipes/${slug}.json`)
       return await response.json()
     }
 
     fetchRecipeJSON().then(json => setRecipe(json))
   }) 
   
-  return (
-    <div>
-      { recipe.metadata.title }
-      { recipe.metadata.author }
-    </div>
-  )
+  if (recipe !== null) {
+    return (
+      <div>
+        { recipe.metadata.title }
+        { recipe.metadata.author }
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        Loading..
+      </div>
+    )
+  }
 }
 
 export default Recipe
