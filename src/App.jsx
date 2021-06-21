ce86d51ac992a0cc25b42a8fafd032c576027c87
Harry Smith <harry@hsmith.net>
2021-06-21 08:51:07 +0100

    React Routing Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams
+} from 'react-router-dom'
+
+import Home from './Home.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <h1 className="text-5xl text-pink-500 font-semibold">
-        Hello, World!
-      </h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/recipe/:slug">
+          <Recipe />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+function Recipe() {
+  let { slug } = useParams();
+  return (
+    <div> { slug } </div>
   )
 }
 
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Smart Recipe Prototype</h1>
+    </div>
+  )
+}
+
+export default Home;
