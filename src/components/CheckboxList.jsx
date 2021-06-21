5302ee37445e28fa10945272b225e4d9e7d854c2
Harry Smith <harry@hsmith.net>
2021-06-21 10:54:03 +0100

    Checkbox List with Tick Marks

--- src/components/CheckboxList.jsx
+++ src/components/CheckboxList.jsx
@@ -1,1 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Switch } from '@headlessui/react'
+
+import { CheckIcon } from '@heroicons/react/outline'
+
+const CheckboxList = ({list}) => {
+  return (
+    <div class="flex flex-col space-y-1">
+      { list.map((listItem) => (
+        <Checkbox item={listItem} key={listItem} />
+      )) }
+    </div>
+  )
+}
+
+const Checkbox = ({item}) => {
+  const [checked, setChecked] = useState(false)
+
+  return (
+    <>
+      <Switch
+        checked={checked}
+        onChange={setChecked}
+      >
+        <div className="flex items-center">
+          <div className="relative">
+            <div className="h-8 w-8 rounded border-2 border-black rotate-3"></div>
+            <CheckIcon className={`${
+              checked ? 'block' : 'hidden'
+            } top-0 left-0 h-8 w-8 absolute text-black-900`} />
+          </div>
+          <p className={`${
+              checked ? 'line-through -rotate-3' : ''
+            } ml-3 text-2xl`}>
+            { item }
+          </p>
+        </div>
+      </Switch>
+    </>
+  )
+}
+
+export default CheckboxList
--- src/components/RecipeIntro.jsx
+++ src/components/RecipeIntro.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import CheckboxList from './CheckboxList.jsx'
+
 const RecipeIntro = ({recipe}) => {
   return (
     <div>
@@ -12,23 +14,27 @@ const RecipeIntro = ({recipe}) => {
           { recipe.metadata.author }
         </p>
         
-        <div class="flex justify-center space-x-2 mt-2">
-          <p class="text-gray-500">
-            <span class="text-gray-600 font-medium">{ recipe.metadata.timings.prep }</span>min preperation
+        <div className="flex justify-center space-x-2 mt-2">
+          <p className="text-gray-500">
+            <span className="text-gray-600 font-medium">{ recipe.metadata.timings.prep }</span>min preperation
           </p>
-          <p class="text-gray-500">
-            <span class="text-gray-600 font-medium">{ recipe.metadata.timings.cook }</span>min cooking
+          <p className="text-gray-500">
+            <span className="text-gray-600 font-medium">{ recipe.metadata.timings.cook }</span>min cooking
           </p>
         </div>
 
         <p>
-          <span class="text-gray-700 font-semibold">{ recipe.metadata.basePortion } </span> 
+          <span className="text-gray-700 font-semibold">{ recipe.metadata.basePortion } </span> 
           portions
         </p>
 
-        <p className="text-2xl text-gray-800 font-medium mt-4">
-          Let's get everything ready!
-        </p>
+        <div className="mt-4">
+          <p className="text-2xl text-gray-800 font-medium">
+            Let's get everything ready!
+          </p>
+
+          <CheckboxList list={["a", "b", "c"]} />
+        </div>
 
       </div>
     </div>
